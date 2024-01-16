78537991149d14446f990fbfdbff0426490d7817
sunny singh <130575872+deVHub91@users.noreply.github.com>
2024-01-16 19:12:24 +0000

    lesson1

--- first.js
+++ first.js
@@ -1,1 +1,16 @@
+const acountId=5679
+let myName="sunny singh"
+var balance="$500"
+console.table([acountId, myName,balance]);
+
+
+myName="aditya raj manu"
+balance="$700"
+
+console.table([acountId, myName,balance]);
+// acountId=5879
+// console.log("acountid");  // const dont allowed to change;
+// prefer not to use var because of block scope and function scope.
+
+
 console.log("sunny")
